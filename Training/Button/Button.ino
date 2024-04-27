c94a4e4882aa9cadd96cc0be30459dedaed5199b
Hyobok Ahn <30744917+dignsys@users.noreply.github.com>
2024-04-27 11:45:07 +0900

    Add files via upload

---
+++ Training/Button/Button.ino
@@ -0,0 +1,19 @@
+#define buttonPin 5
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
+  Serial.print("Button pin : "); Serial.println(buttonPin); 
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  if (buttonState == HIGH) {      
+    Serial.println("Button input value High");
+  } else {    
+    Serial.println("Button input value Low");
+  }
+  delay(500);
+}
